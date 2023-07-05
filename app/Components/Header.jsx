bb79aaed3d343ab3fbe366ac817352b3146adab6
Bianca <bianca.milanez@kcl.ac.uk>
2023-07-05 20:43:43 +0100

    Medium/small screens heading

--- app/Components/Header.jsx
+++ app/Components/Header.jsx
@@ -4,12 +4,12 @@ export default function Header() {
             <nav className='flex justify-between'>
             <h2 className="text-xl font-funky">run by KCL Robotics</h2>
             <ul className='flex items-center gap-2'>
-                <button className="font-bold text-md text-textColour px-4 py-2 rounded-lg bg-slate-200 shadow-md
-                    hover:bg-slate-100 focus:outline-none focus:ring focus:ring-textColour"> Menu</button> 
-                <button className="font-bold text-md text-textColour px-4 py-2 rounded-lg bg-slate-200 shadow-md
-                    hover:bg-slate-100 focus:outline-none focus:ring focus:ring-textColour"> Log in</button>
-                <button className="font-bold text-md text-textColour px-4 py-2 rounded-lg bg-slate-200 shadow-md
-                    hover:bg-slate-100 focus:outline-none focus:ring focus:ring-textColour"> Register</button>
+                <button className="font-bold text-sm text-textColour px-2 py-1 rounded-lg bg-slate-200 shadow-md
+                    hover:bg-slate-100 focus:outline-none focus:ring focus:ring-textColour lg:text-md lg:px-4 lg:py-2"> Menu</button> 
+                <button className="font-bold text-sm text-textColour px-2 py-1 rounded-lg bg-slate-200 shadow-md
+                    hover:bg-slate-100 focus:outline-none focus:ring focus:ring-textColour lg:text-md lg:px-4 lg:py-2"> Login</button>
+                <button className="font-bold text-sm text-textColour px-2 py-1 rounded-lg bg-slate-200 shadow-md
+                    hover:bg-slate-100 focus:outline-none focus:ring focus:ring-textColour lg:text-md lg:px-4 lg:py-2"> Register</button>
             </ul>
             </nav>   
         </div>
